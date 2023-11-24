4f40a334d2547a34b3433c2ca1f9b5e0bb2bb844
saji2000 <sajaddaneshmand79@gmail.com>
2023-11-23 21:18:59 -0700

    use state review

--- src/App.js
+++ src/App.js
@@ -1,85 +1,12 @@
-import "./App.css";
-import { ThemeProvider, useTheme } from "./Components/ThemeContext";
-import Switch from "./Switch";
+import { useState } from "react";
 
-const Title = ({ children }) => {
-  const { theme } = useTheme();
-  return (
-    <h2
-      style={{
-        color: theme === "light" ? "black" : "white",
-      }}
-    >
-      {children}
-    </h2>
-  );
-};
+export default function App() {
+  const [resturant, setResturant] = useState("Little Lemon");
 
-const Paragraph = ({ children }) => {
-  const { theme } = useTheme();
-  return (
-    <p
-      style={{
-        color: theme === "light" ? "black" : "white",
-      }}
-    >
-      {children}
-    </p>
-  );
-};
-
-const Content = () => {
   return (
     <div>
-      <Paragraph>
-        We are a pizza loving family. And for years, I searched and searched and
-        searched for the perfect pizza dough recipe. I tried dozens, or more.
-        And while some were good, none of them were that recipe that would make
-        me stop trying all of the others.
-      </Paragraph>
-    </div>
-  );
-};
-
-const Header = () => {
-  return (
-    <header>
-      <Title>Little Lemon 🍕</Title>
-      <Switch />
-    </header>
-  );
-};
-
-const Page = () => {
-  return (
-    <div className="Page">
-      <Title>When it comes to dough</Title>
-      <Content />
-    </div>
-  );
-};
-
-function App() {
-  const { theme } = useTheme();
-  return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === "light" ? "white" : "black",
-      }}
-    >
-      <Header />
-      <Page />
+      <h1>{resturant}</h1>
+      <button onClick={() => setResturant("Checkers")}>Change Resturant</button>
     </div>
   );
 }
-
-function Root() {
-  return (
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  );
-}
-
-export default Root;
