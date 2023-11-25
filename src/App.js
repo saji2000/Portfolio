eab14a769e4fbe8b6407e8a23f14e6c084c95973
saji2000 <sajaddaneshmand79@gmail.com>
2023-11-24 19:53:40 -0700

    use state review

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { useState } from "react";
 
 export default function App() {
-  const [resturant, setResturant] = useState("Little Lemon");
+  const [resturant, setResturant] = useState({
+    resturant: "Little Lemon",
+    city: "Edmonton",
+  });
+
+  function updateResturant() {
+    setResturant((prevState) => {
+      return { ...prevState, resturant: "Checkers" };
+    });
+  }
 
   return (
     <div>
-      <h1>{resturant}</h1>
-      <button onClick={() => setResturant("Checkers")}>Change Resturant</button>
+      <h1>
+        {resturant.resturant} is located in {resturant.city}
+      </h1>
+      <button onClick={updateResturant}>Change Resturant</button>
     </div>
   );
 }
